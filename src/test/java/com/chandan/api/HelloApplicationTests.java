03830f991c10050524089d0d4aeb2e1ef98742e3
Chandan D S <chandandhanush273747@gmail.com>
2024-08-15 11:51:52 +0530

    Hello api

---
+++ src/test/java/com/chandan/api/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandan.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
